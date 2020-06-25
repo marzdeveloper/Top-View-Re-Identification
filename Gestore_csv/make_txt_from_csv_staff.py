5d9497ed0f88c934316a0574b23312552d0a90eb
marzdeveloper <daniel.23@live.it>
2020-06-25 17:15:21 +0200

    add make_txt_from_csv_staff

---
+++ Gestore_csv/make_txt_from_csv_staff.py
@@ -0,0 +1,116 @@
+import csv
+import random
+
+path = "C:/Users/Daniele/Desktop/Dataset_gennaio/csv_update/result_.csv" #path al csv
+txt_path = "C:/Users/Daniele/Desktop/prova/"
+
+min = 100 #numero minimo di foto per classe
+max = 100 #numero massimo di foto per classe
+
+max_id_train=46
+max_id_test=100
+i = 0
+count = 0
+in_file = open(path)
+csvreader = csv.reader(in_file, delimiter=";")
+train_file = open(txt_path+"train.txt", "w", newline='')
+val_file = open(txt_path+"val.txt", "w", newline='')
+test_file = open(txt_path+"test.txt", "w", newline='')
+staff = [1,8,9,144]
+staff_train = []
+people_train = []
+staff_dict = {}
+
+for row in csvreader:
+    if (len(row) - 3) > (min - 1):
+        if (len(row) - 3) > max:
+            max_foto = max
+        else:
+            max_foto = (len(row) - 3)
+        directory = row[1]
+        original = row[0]
+        row = row[3:]
+        apache = random.sample(row, min)
+        if int(directory) not in staff:
+            for i, photo in enumerate(apache):
+                people_train.append(int(original))
+                if i < int(0.7*max):#max o max_foto ?
+                    train_file.write(directory + '/' + photo.strip('_rgb.png') + ' ' + str(count) + '\n')
+                elif i >= int(0.9*max):
+                    test_file.write(directory + '/' + photo.strip('_rgb.png') + ' ' + str(count) + '\n')
+                else:
+                    val_file.write(directory + '/' + photo.strip('_rgb.png') + ' ' + str(count) + '\n')
+            count += 1
+        if count == max_id_train:
+            break
+
+max_id_train = 50
+
+in_file.seek(0)
+for row in csvreader:
+    if (len(row) - 3) > (min - 1):
+        if (len(row) - 3) > max:
+            max_foto = max
+        else:
+            max_foto = (len(row) - 3)
+        directory = row[1]
+        original = row[0]
+        if int(directory) in staff:
+            row = row[3:]
+            apache = random.sample(row, min)
+            for i, photo in enumerate(apache):
+                staff_train.append(int(original))
+                if i < int(0.8*max):#max o max_foto ?
+                    train_file.write(directory + '/' + photo.strip('_rgb.png') + ' ' + str(count) + '\n')
+                else:
+                    val_file.write(directory + '/' + photo.strip('_rgb.png') + ' ' + str(count) + '\n')
+            staff_dict[directory] = count
+            staff.remove(int(directory))
+            count += 1
+        if count == max_id_train:
+            break
+min = 10 #numero minimo di foto per classe
+max = 10 #numero massimo di foto per classe
+staff = [1,8,9,144]
+
+in_file.seek(0)
+for row in csvreader:
+    if (len(row) - 3) > (min - 1):
+        if (len(row) - 3) > max:
+            max_foto = max
+        else:
+            max_foto = (len(row) - 3)
+        directory = row[1]
+        original = row[0]
+        if not (int(original) in people_train and int(directory) in staff):
+            row = row[3:]
+            apache = random.sample(row, min)
+            for i, photo in enumerate(apache):
+                test_file.write(directory + '/' + photo.strip('_rgb.png') + ' ' + str(count) + '\n')                #mette nel test la gente normale
+            count += 1
+        if count == max_id_test:
+            break
+
+min = 50 #numero minimo di foto per classe
+max = 50 #numero massimo di foto per classe
+
+in_file.seek(0)
+for row in csvreader:
+    if (len(row) - 3) > (min - 1):
+        if (len(row) - 3) > max:
+            max_foto = max
+        else:
+            max_foto = (len(row) - 3)
+        directory = row[1]
+        original = row[0]
+        if directory in staff:
+            if not (int(original) in staff_train):
+                row = row[3:]
+                apache = random.sample(row, min)
+                for i, photo in enumerate(apache):
+                    test_file.write(directory + '/' + photo.strip('_rgb.png') + ' ' + str(staff_dict[directory]) + '\n')            #mette nel test lo staff in giorni diversi dal training
+                staff.remove(int(directory))
+
+
+print("Numero di classi: " + str(count))
+print("Staff dict", staff_dict)
