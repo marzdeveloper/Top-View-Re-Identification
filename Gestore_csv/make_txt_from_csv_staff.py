25937fa7fbfc102b2fe5bd21b9b9fb6da1f85627
lor-cat <62765277+lor-cat@users.noreply.github.com>
2020-06-25 17:40:58 +0200

    update

--- Gestore_csv/make_txt_from_csv_staff.py
+++ Gestore_csv/make_txt_from_csv_staff.py
@@ -1,26 +1,32 @@
 import csv
 import random
 
-path = "C:/Users/Daniele/Desktop/Dataset_gennaio/csv_update/result_.csv" #path al csv
-txt_path = "C:/Users/Daniele/Desktop/prova/"
+path = "D:/DATASET/csv_con_staff_separato/csv genn staff separato/result_.csv" #path al csv
+txt_path = "D:/DATASET/csv_con_staff_separato/csv genn staff separato/"
+csv_dest_path = "D:/DATASET/csv_con_staff_separato/csv genn staff separato/result_gennaio_staff_separato.csv"
 
 min = 100 #numero minimo di foto per classe
 max = 100 #numero massimo di foto per classe
 
-max_id_train=46
-max_id_test=100
+max_id_train=46 #max num of train id
+max_id_test=100 #max num of test id
+
 i = 0
-count = 0
+count = 0   #contatore id aggiunti
+
 in_file = open(path)
 csvreader = csv.reader(in_file, delimiter=";")
 train_file = open(txt_path+"train.txt", "w", newline='')
 val_file = open(txt_path+"val.txt", "w", newline='')
 test_file = open(txt_path+"test.txt", "w", newline='')
-staff = [1,8,9,144]
-staff_train = []
-people_train = []
-staff_dict = {}
 
+staff = [1,8,9,144] #contiene gli id dello staff che si vuole aggiungere
+staff_train = []    #id originale dello staff
+people_train = []   #id originale delle persone
+staff_dict = {}     #associa staff a staff train
+
+
+#scan per persone normali
 for row in csvreader:
     if (len(row) - 3) > (min - 1):
         if (len(row) - 3) > max:
@@ -45,8 +51,8 @@ for row in csvreader:
             break
 
 max_id_train = 50
-
 in_file.seek(0)
+#scan per train e val dello staff
 for row in csvreader:
     if (len(row) - 3) > (min - 1):
         if (len(row) - 3) > max:
@@ -69,10 +75,14 @@ for row in csvreader:
             count += 1
         if count == max_id_train:
             break
+
+
+#numero foto test per id
 min = 10 #numero minimo di foto per classe
 max = 10 #numero massimo di foto per classe
 staff = [1,8,9,144]
 
+#scan per aggiungere persone nel test che non compaiono nel train e val
 in_file.seek(0)
 for row in csvreader:
     if (len(row) - 3) > (min - 1):
@@ -85,7 +95,7 @@ for row in csvreader:
         if not (int(original) in people_train and int(directory) in staff):
             row = row[3:]
             apache = random.sample(row, min)
-            for i, photo in enumerate(apache):
+            for photo in apache:
                 test_file.write(directory + '/' + photo.strip('_rgb.png') + ' ' + str(count) + '\n')                #mette nel test la gente normale
             count += 1
         if count == max_id_test:
@@ -93,8 +103,8 @@ for row in csvreader:
 
 min = 50 #numero minimo di foto per classe
 max = 50 #numero massimo di foto per classe
-
 in_file.seek(0)
+#scan per aggiungere giorni dello staff nel test che non compaiono nel train e val
 for row in csvreader:
     if (len(row) - 3) > (min - 1):
         if (len(row) - 3) > max:
@@ -107,7 +117,7 @@ for row in csvreader:
             if not (int(original) in staff_train):
                 row = row[3:]
                 apache = random.sample(row, min)
-                for i, photo in enumerate(apache):
+                for photo in apache:
                     test_file.write(directory + '/' + photo.strip('_rgb.png') + ' ' + str(staff_dict[directory]) + '\n')            #mette nel test lo staff in giorni diversi dal training
                 staff.remove(int(directory))
 
